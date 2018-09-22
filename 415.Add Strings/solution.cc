8b054c6921101dd10887f14002922cc71d812f34
cshunde <cshunde@foxmail.com>
2018-09-22 19:05:19 +0800

    improve performance: replace mod and divide operation

--- 415.Add Strings/solution.cc
+++ 415.Add Strings/solution.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 class Solution {
-public:
+ public:
   string addStrings(string num1, string num2) {
     string result;
     result.reserve(std::max(num1.size() + 1, num2.size() + 1));
@@ -11,20 +11,32 @@ public:
     string::const_reverse_iterator iter1 = num1.crbegin();
     string::const_reverse_iterator iter2 = num2.crbegin();
     for (; iter1 != num1.crend() && iter2 != num2.crend(); iter1++, iter2++) {
-      int sum = (*iter1 - '0') + (*iter2 - '0') + carry;
-      carry = sum / 10;
-      result.push_back((sum % 10) + '0');
+      int q = (*iter1 - '0') + (*iter2 - '0') + carry;
+      carry = 0;
+      if (q >= 10) {
+        q -= 10;
+        carry = 1;
+      }
+      result.push_back(q + '0');
     }
     while (iter1 != num1.crend()) {
-      int sum = (*iter1 - '0') + carry;
-      carry = sum / 10;
-      result.push_back((sum % 10) + '0');
+      int q = (*iter1 - '0') + carry;
+      carry = 0;
+      if (q >= 10) {
+        q -= 10;
+        carry = 1;
+      }
+      result.push_back(q + '0');
       iter1++;
     }
     while (iter2 != num2.crend()) {
-      int sum = (*iter2 - '0') + carry;
-      carry = sum / 10;
-      result.push_back((sum % 10) + '0');
+      int q = (*iter2 - '0') + carry;
+      carry = 0;
+      if (q >= 10) {
+        q -= 10;
+        carry = 1;
+      }
+      result.push_back(q + '0');
       iter2++;
     }
     if (carry != 0) {
