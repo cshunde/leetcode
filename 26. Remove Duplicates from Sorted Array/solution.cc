563f610cc27c019ee42252165a3267fc85496d85
cshunde <cshunde@foxmail.com>
2018-09-26 23:57:49 +0800

    p26: pass

---
+++ 26. Remove Duplicates from Sorted Array/solution.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return nums.size();
+        }
+        int len = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[len-1]) {
+                continue;
+            } 
+            nums[len] = nums[i];
+            len++;
+        }
+        return len;
+    }
+};
